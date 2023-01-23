686de90270028191aa5cb4b7674158a1206fb9b5
gabrielbritog <gabriel.britog@outlook.com>
2023-01-23 11:23:10 -0300

    Refatorando e aplicando CSS

--- SPA/src/pages/home/index.js
+++ SPA/src/pages/home/index.js
@@ -1,12 +1,12 @@
 export default () =>{
     const container = document.createElement('div');
     const template = `
-    <h2>Um texto Teste</h2>
+    <center><h2>Um texto Teste</h2></center>
     <p>Lorem ipsum bibendum fames velit volutpat ultricies bibendum faucibus elit, 
     convallis praesent netus tristique congue senectus curabitur libero, suscipit id 
     at mollis fames aenean cras imperdiet. suspendisse platea bibendum donec egestas metus
      pulvinar id sit amet, mattis est vehicula aliquam ante etiam facilisis aliquet aptent </p>
-     <p>Teste SPA</p>
+     <center><p>Teste SPA</p></center>
     `;
     container.innerHTML = template;
     return container;   
