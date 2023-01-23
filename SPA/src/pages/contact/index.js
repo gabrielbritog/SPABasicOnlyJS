89b7eaf3c678fd3571fe035564fc5fd09ee05c39
gabrielbritog <gabriel.britog@outlook.com>
2023-01-23 10:53:44 -0300

    Primeiro Commit Spa

---
+++ SPA/src/pages/contact/index.js
@@ -0,0 +1,13 @@
+export default () =>{
+    const container = document.createElement('div');
+    const template = `
+    <h2>Contato</h2>
+    <p>Lorem ipsum bibendum fames velit volutpat ultricies bibendum faucibus elit, 
+    convallis praesent netus tristique congue senectus curabitur libero, suscipit id 
+    at mollis fames aenean cras imperdiet. suspendisse platea bibendum donec egestas metus
+     pulvinar id sit amet, mattis est vehicula aliquam ante etiam facilisis aliquet aptent </p>
+     <p>Teste SPA</p>
+    `;
+    container.innerHTML = template;
+    return container;   
+}
---
+++ routes.js
@@ -0,0 +1,9 @@
+import home from "./SPA/src/pages/home/index.js";
+import contacts from "./SPA/src/pages/contact/index.js";
+import about from "./SPA/src/pages/about/index.js";
+
+export default {
+  home: home(),
+  contacts: contacts(),
+  about: about()
+}
